e47a4d9d501a8f96d9c0fd92f08d5d5356ac5041
Nekromorfo20 <alan2304@gmail.com>
2024-02-20 02:29:17 -0600

    Cambios finales al proyecto

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import {
     Meta,
     Links,
@@ -54,9 +55,60 @@ export function links () {
 }
 
 export default function App() {
+    // Colocar en localStorage un arreglo vacio cuando la aplicacion carga por primera vez
+    const carritoLS = typeof window !== "undefined" ? JSON.parse(localStorage.getItem("carrito")) ?? [] : []
+    
+    // Variables para OutletContaxt (Variables globales)
+    const [carrito, setCarrito] = useState(carritoLS)
+
+    useEffect(() => {
+        if (carrito?.length === 0) return
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+    }, [carrito])
+
+    const agregarCarrito = guitarra => {
+        if (carrito.some(guitarraState => guitarraState.id === guitarra.id)) {
+            // Iterar sobre el arreglo e identificar el elemento duplicado
+            const carritoActualizado = carrito.map(guitarraState => {
+                if (guitarraState.id === guitarra.id) {
+                    // Reescribir la cantidad
+                    guitarraState.cantidad = guitarra.cantidad
+                }
+                return guitarraState
+            })
+            // Añadir al carrito
+            setCarrito(carritoActualizado)
+        } else {
+            // Registro nuevo agregar al carrito
+            setCarrito([...carrito, guitarra])
+        }
+    }
+
+    const actualizarCantidad = guitarra => {
+        const carritoActualizado = carrito.map(guitarraState => {
+            if (guitarraState.id === guitarra.id) {
+                guitarraState.cantidad = guitarra.cantidad
+            }
+            return guitarraState
+        })
+        setCarrito(carritoActualizado)
+    }
+
+    const eliminarGuitarra = id => {
+        const carritoActualizado = carrito.filter(guitarraState => guitarraState.id !== id)
+        setCarrito(carritoActualizado)
+    }
+
     return (
         <Document>
-            <Outlet />
+            <Outlet
+                context={{
+                    agregarCarrito,
+                    carrito,
+                    actualizarCantidad,
+                    eliminarGuitarra
+                }}
+            />
         </Document>
     )
 }
--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from "@remix-run/react"
+import { useState } from "react"
+import { useLoaderData, useOutletContext } from "@remix-run/react"
 import { getGuitarra } from "~/models/guitarras.server"
 
 export async function loader ({ params }) {
@@ -39,8 +40,29 @@ export function meta ({ data }) {
 }
 
 const Guitarra = () => {
+  const { agregarCarrito } = useOutletContext()
+  const [cantidad, setCantidad] = useState(0)
   const guitarra = useLoaderData()
   const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    
+    if (cantidad < 1) {
+        alert("Debes seleccionar una cantidad")
+        return null
+    }
+
+    const guitarraSeleccionada = {
+        id: guitarra.data[0].id,
+        imagen: imagen.data.attributes.url,
+        nombre,
+        precio,
+        cantidad
+    }
+
+    agregarCarrito(guitarraSeleccionada)
+  }
 
   return (
     <div className="guitarra">
@@ -50,6 +72,26 @@ const Guitarra = () => {
             <h3>{nombre}</h3>
             <p className="texto">{descripcion}</p>
             <p className="precio">{precio}</p>
+
+            <form onSubmit={handleSubmit} className="formulario">
+                <label htmlFor="cantidad">Cantidad</label>
+                <select
+                  onChange={e => setCantidad(+e.target.value)}
+                  id="cantidad"
+                >
+                    <option value="0">-- Seleccione --</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+
+                <input
+                    type="submit"
+                    value="Agregar al carrito"
+                />
+            </form>
         </div>
     </div>
   )
